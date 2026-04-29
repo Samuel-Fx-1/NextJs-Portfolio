b9d29f0fdfa942aeb0e6d9953313785b0e39705e
samuel-fx-1 <samuelislegend8.com>
2026-04-29 12:37:36 +0200

    Update:SamPort

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,6 +15,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
+          {/* ADD THIS LINE ONLY */}
+          <h1  className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-cyan-400 to-blue-600 hover:from-cyan-500 hover:to-blue-700 text-white font-semibold"
+            >
+              Welcome to My Portfolio
+          </h1>
+          <br />
+          
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
@@ -22,7 +29,7 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "I.Samuel",
                 1000,
                 "Web Developer",
                 1000,
@@ -36,6 +43,8 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
+        <p>FULL STACK DEVELOPER</p>
+        
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
             voluptuous.
@@ -49,11 +58,10 @@ const HeroSection = () => {
             </Link>
             <Link
               href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
-              </span>
+              view my portfolio
+              
             </Link>
           </div>
         </motion.div>
